aa258ae287b8c0827d2eaa2cbc2c463c6fe22a6b
liamjbennett <liam.j.bennett@gmail.com>
2014-10-17 13:33:42 +0100

    adding support for 4.5.1 and 4.5.2

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,6 +24,16 @@ class dotnet::params {
       'url' => 'http://download.microsoft.com/download/b/a/4/ba4a7e71-2906-4b2d-a0e1-80cf16844f5f/dotnetfx45_full_x86_x64.exe',
       'exe' => 'dotnetfx45_full_x86_x64.exe',
       'key' => 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1AD147D0-BE0E-3D6C-AC11-64F6DC4163F1}'
+    },
+    '4.5.1' => {
+      'url' => 'http://download.microsoft.com/download/1/6/7/167F0D79-9317-48AE-AEDB-17120579F8E2/NDP451-KB2858728-x86-x64-AllOS-ENU.exe'
+      'exe' => 'NDP451-KB2858728-x86-x64-AllOS-ENU.exe',
+      'key' => 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{7DEBE4EB-6B40-3766-BB35-5CBBC385DA37}'
+    },
+    '4.5.2' => {
+      'url' => 'http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe'
+      'exe' => 'NDP451-KB2858728-x86-x64-AllOS-ENU.exe',
+      'key' => 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{26784146-6E05-3FF9-9335-786C7C0FB5BE}'
     }
   }
 }
