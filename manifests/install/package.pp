f8623cb478d7633f745465819ab3d6a8f65bc1ab
Yosef <yosef@yosef.net>
2015-02-27 13:37:49 -0600

    fixed variable assignment.

--- manifests/install/package.pp
+++ manifests/install/package.pp
@@ -10,25 +10,31 @@ define dotnet::install::package(
   include dotnet::params
 
   if("x${url}x" == 'xx'){
-    $url = $dotnet::params::version[$version]['url']
+    $package_url = $dotnet::params::version[$version]['url']
+  } else {
+    $package_url = $url
   }
   if("x${exe}x" == 'xx'){
-    $exe = $dotnet::params::version[$version]['exe']
+    $package_exe = $dotnet::params::version[$version]['exe']
+  } else {
+    $package_exe = $exe
   }
   if("x${key}x" == 'xx'){
-    $key = $dotnet::params::version[$version]['key']
+    $package_key = $dotnet::params::version[$version]['key']
+  } else {
+    $package_key = $key
   }
 
   if "x${package_dir}x" == 'xx' {
     $source_dir = "C:\\Windows\\Temp"
     if $ensure == 'present' {
       download_file { "download-dotnet-${version}" :
-        url                   => $url,
+        url                   => $package_url,
         destination_directory => $source_dir,
         before                => Exec["configure-dotnet-${version}-${ensure}"],
       }
     } else {
-      file { "C:/Windows/Temp/${exe}":
+      file { "C:/Windows/Temp/${package_exe}":
         ensure => absent,
       }
     }
@@ -38,18 +44,18 @@ define dotnet::install::package(
 
   if $ensure == 'present' {
     exec { "configure-dotnet-${version}-${ensure}":
-      command   => "& ${source_dir}\\${exe} /q /norestart",
+      command   => "& ${source_dir}\\${package_exe} /q /norestart",
       provider  => powershell,
       logoutput => true,
-      unless    => "if ((Get-Item -LiteralPath \'${key}\' -ErrorAction SilentlyContinue).GetValue(\'DisplayVersion\')) { exit 0 }",
+      unless    => "if ((Get-Item -LiteralPath \'${package_key}\' -ErrorAction SilentlyContinue).GetValue(\'DisplayVersion\')) { exit 0 }",
 
     }
   } else {
     exec { "configure-dotnet-${version}-${ensure}":
-      command   => "& ${source_dir}\\${exe} /x /q /norestart",
+      command   => "& ${source_dir}\\${package_exe} /x /q /norestart",
       provider  => powershell,
       logoutput => true,
-      unless    => "if ((Get-Item -LiteralPath \'${key}\' -ErrorAction SilentlyContinue).GetValue(\'DisplayVersion\')) { exit 1 }",
+      unless    => "if ((Get-Item -LiteralPath \'${package_key}\' -ErrorAction SilentlyContinue).GetValue(\'DisplayVersion\')) { exit 1 }",
 
     }
   }
