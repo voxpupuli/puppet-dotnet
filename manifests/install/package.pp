663561170e2de1d157a3732b8451a9aa4f2e84b4
Reid Vandewiele <reid@puppetlabs.com>
2015-09-21 10:16:08 -0700

    Add logic to prevent incompatible versions
    Some versions of .NET are in-place upgrades of others. Installation of
    .NET 4.5, for example, will replace the 4.0 package. In order to
    disallow Puppet from continuously trying to install .NET 4.0 in the
    event dotnet resources for both 4.0 and 4.5 have been added to the
    catalog, create a package=absent resource for each conflicting version.
    This will cause the conflict to be caught when a catalog is compiled for
    the node.

--- manifests/install/package.pp
+++ manifests/install/package.pp
@@ -7,9 +7,10 @@ define dotnet::install::package(
 
   include dotnet::params
 
-  $url     = $dotnet::params::version[$version]['url']
-  $exe     = $dotnet::params::version[$version]['exe']
-  $package = $dotnet::params::version[$version]['package']
+  $url       = $dotnet::params::version[$version]['url']
+  $exe       = $dotnet::params::version[$version]['exe']
+  $package   = $dotnet::params::version[$version]['package']
+  $conflicts = $dotnet::params::version[$version]['conflicts']
 
   if "x${package_dir}x" == 'xx' {
     $source_file = "C:/Windows/Temp/${exe}"
@@ -26,6 +27,22 @@ define dotnet::install::package(
     source            => $source_file,
     install_options   => ['/q', '/norestart'],
     uninstall_options => ['/x', '/q', '/norestart'],
+  }
+
+  if $ensure == 'present' {
+    # Some versions of .NET are in-place upgrades of others. Installation of
+    # .NET 4.5, for example, will replace the 4.0 package. In order to disallow
+    # Puppet from continuously trying to install .NET 4.0 in the event dotnet
+    # resources for both 4.0 and 4.5 have been added to the catalog, create a
+    # package=absent resource for each conflicting version. This will cause the
+    # conflict to be caught when a catalog is compiled for the node.
+    $conflicts.each |$conflict| {
+      package { $dotnet::params::version[$conflict]['package']:
+        ensure            => absent,
+        before            => Package[$package],
+        uninstall_options => ['/x', '/q', '/norestart'],
+      }
+    }
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -11,32 +11,37 @@ class dotnet::params {
 
   $version = {
     '3.5' => {
-      'url'     => 'http://download.microsoft.com/download/7/0/3/703455ee-a747-4cc8-bd3e-98a615c3aedb/dotNetFx35setup.exe',
-      'exe'     => 'dotNetFx35setup.exe',
-      'package' => [ ],
+      'url'       => 'http://download.microsoft.com/download/7/0/3/703455ee-a747-4cc8-bd3e-98a615c3aedb/dotNetFx35setup.exe',
+      'exe'       => 'dotNetFx35setup.exe',
+      'conflicts' => [ ],
+      'package'   => [ ],
     },
     '4.0' => {
-      'url'     => 'http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe',
-      'exe'     => 'dotNetFx40_Full_x86_x64.exe',
-      'package' => [
+      'url'       => 'http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe',
+      'exe'       => 'dotNetFx40_Full_x86_x64.exe',
+      'conflicts' => ['4.5', '4.5.1', '4.5.2'],
+      'package'   => [
         'Microsoft .NET Framework 4 Extended',
         'Microsoft .NET Framework 4 Client Profile',
       ],
     },
-    '4.5' => {
-      'url'     => 'http://download.microsoft.com/download/b/a/4/ba4a7e71-2906-4b2d-a0e1-80cf16844f5f/dotnetfx45_full_x86_x64.exe',
-      'exe'     => 'dotnetfx45_full_x86_x64.exe',
-      'package' => 'Microsoft .NET Framework 4.5',
+    '4.5'         => {
+      'url'       => 'http://download.microsoft.com/download/b/a/4/ba4a7e71-2906-4b2d-a0e1-80cf16844f5f/dotnetfx45_full_x86_x64.exe',
+      'exe'       => 'dotnetfx45_full_x86_x64.exe',
+      'conflicts' => ['4.0', '4.5.1', '4.5.2'],
+      'package'   => 'Microsoft .NET Framework 4.5',
     },
     '4.5.1' => {
-      'url'     => 'http://download.microsoft.com/download/1/6/7/167F0D79-9317-48AE-AEDB-17120579F8E2/NDP451-KB2858728-x86-x64-AllOS-ENU.exe',
-      'exe'     => 'NDP451-KB2858728-x86-x64-AllOS-ENU.exe',
-      'package' => 'Microsoft .NET Framework 4.5.1',
+      'url'       => 'http://download.microsoft.com/download/1/6/7/167F0D79-9317-48AE-AEDB-17120579F8E2/NDP451-KB2858728-x86-x64-AllOS-ENU.exe',
+      'exe'       => 'NDP451-KB2858728-x86-x64-AllOS-ENU.exe',
+      'conflicts' => ['4.0', '4.5', '4.5.2'],
+      'package'   => 'Microsoft .NET Framework 4.5.1',
     },
     '4.5.2' => {
-      'url'     => 'http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe',
-      'exe'     => 'NDP452-KB2901907-x86-x64-AllOS-ENU.exe',
-      'package' => 'Microsoft .NET Framework 4.5.2',
+      'url'       => 'http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe',
+      'exe'       => 'NDP452-KB2901907-x86-x64-AllOS-ENU.exe',
+      'conflicts' => ['4.0', '4.5', '4.5.1'],
+      'package'   => 'Microsoft .NET Framework 4.5.2',
     }
   }
 
